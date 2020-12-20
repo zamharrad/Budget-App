94dffd56e5cdb446ef4c2d7a5ac59ca5e8a2fc87
zamharrad <mhmdzamhar@gamil.com>
2020-12-20 19:15:01 +0530

    added chnages

--- app.js
+++ app.js
@@ -11,15 +11,20 @@ var controller = (function (budgetCtrl, UICntr) {
   document.querySelector(".add__btn").addEventListener("click", function () {
     console.log("button was clicked");
 
-   //  1. Get the filed input data
+    //  1. Get the filed input data
 
-   // 2. Add the item to the budget
+    // 2. Add the item to the budget
 
-   //  3. Add the item to UI
+    //  3. Add the item to UI
 
-   //  4. Calculate the budget
+    //  4. Calculate the budget
 
-   //  5. Display the budget on UI
+    //  5. Display the budget on UI
+  });
 
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+      console.log("entered");
+    }
   });
 })(bugetController, UIController);
